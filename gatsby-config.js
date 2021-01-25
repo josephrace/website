fc59b5c92964e72a5f217d920c4f711ac116f453
Joe Race <2642432+josephrace@users.noreply.github.com>
2021-01-25 09:10:40 +0000

    Remove Work Sans and upate word cloud (#12)

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-fonts',
       options: {
-        fonts: ['Work Sans', 'PT Serif'],
+        fonts: ['PT Serif:400'],
       },
     },
     {
